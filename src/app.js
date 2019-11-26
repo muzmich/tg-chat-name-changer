4e8f8f80c4e33a1c7c68f5bbbdf358b7288fb470
Misha <muuuzmich@gmail.com>
2019-11-27 01:46:01 +0200

    Chat title changes on start

--- src/app.js
+++ src/app.js
@@ -6,28 +6,29 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 const rest = express();
 
 rest.use(express.json());
-rest.use(express.urlencoded({ extended: false }));
+rest.use(express.urlencoded({extended: false}));
 
-let intervals = {};
-let chats = {};
+let intervals = {},
+    chats = {};
+
+async function changeTitle(ctx) {
+    const newName = await generateName();
+    console.log(`[${ctx.chat.id}] New name: ${newName}`);
+    bot.telegram.setChatTitle(ctx.chat.id, newName);
+}
+
 bot.command('start', ctx => {
-    intervals[ctx.chat.id] = setInterval(async function () {
-        const newName = await generateName();
-        console.log(`[${ctx.chat.id}] New name: ${newName}`)
-        bot.telegram.setChatTitle(ctx.chat.id, newName);
-    }, 12 * 60 * 60 * 1000);
-
-    if (!chats[ctx.chat.id.toString()]) {
+    intervals[ctx.chat.id] = setInterval(() => changeTitle(ctx), 12 * 60 * 60 * 1000);
+    if (!chats[ctx.chat.id.toString()])
         chats[ctx.chat.id] = ctx.chat;
-    }
 
-    console.log(`[${ctx.chat.id}] Started`)
-    return ctx.reply('Ууу сук, ща как буду раз в час имена менять');
+    ctx.reply('Ууу сук, ща как буду раз в 12 часов имена менять').then(changeTitle(ctx));
+    console.log(`[${ctx.chat.id}] Started`);
 });
 
 bot.command('stop', ctx => {
     clearInterval(intervals[ctx.chat.id]);
-    console.log(`[${ctx.chat.id}] Stopped`)
+    console.log(`[${ctx.chat.id}] Stopped`);
     return ctx.reply('Всё, больше не буду');
 });
 
