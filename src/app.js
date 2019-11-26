420d66e3a539244aa10c5aa0ffb5b73b7fd75d1c
Vladyslav Berezhnyi <vladyslav.berezhnyi@technorely.com>
2019-11-27 01:24:33 +0200

    Release

--- src/app.js
+++ src/app.js
@@ -3,23 +3,37 @@ const generateName = require('./components/generateName');
 const express = require('express');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+const rest = express();
+
+rest.use(express.json());
+rest.use(express.urlencoded({ extended: false }));
 
 let intervals = {};
+let chats = {};
 bot.command('start', ctx => {
     intervals[ctx.chat.id] = setInterval(async function () {
         const newName = await generateName();
-        console.log(`New name for chat ${ctx.chat.id} - ${newName}`)
+        console.log(`[${ctx.chat.id}] New name: ${newName}`)
         bot.telegram.setChatTitle(ctx.chat.id, newName);
-    }, 10 * 1000);
-    console.log(`Changing for chat ${ctx.chat.id} started`)
+    }, 12 * 60 * 60 * 1000);
+
+    if (!chats[ctx.chat.id.toString()]) {
+        chats[ctx.chat.id] = ctx.chat;
+    }
+
+    console.log(`[${ctx.chat.id}] Started`)
     return ctx.reply('Ууу сук, ща как буду раз в час имена менять');
 });
 
 bot.command('stop', ctx => {
     clearInterval(intervals[ctx.chat.id]);
-    console.log(`Changing for chat ${ctx.chat.id} stopped`)
+    console.log(`[${ctx.chat.id}] Stopped`)
     return ctx.reply('Всё, больше не буду');
 });
 
+rest.get('/chats', (req, res) => {
+    res.send(chats);
+});
+
 bot.launch().then(() => console.log('Bot is up'));
-express().listen(process.env.PORT || 3000);
+rest.listen(process.env.PORT || 3000);
